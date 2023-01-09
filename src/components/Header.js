a1f0e75635d733ec18323322b2efe80cf7010bac
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-01-09 14:45:59 +0100

    Logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 
 import MyButton from './Button';
+import logo from './logo.png'
 
 
 export default function Header() {
@@ -10,11 +11,12 @@ export default function Header() {
     return (
         <div className="header">
             <div className="logo">
+                <img src={logo} alt='img'/>
             </div>
             <div className="navbar">
-                <MyButton text={buttontext1}/>
-                <MyButton text={buttontext2}/>      
-                <MyButton text={buttontext3}/>
+                <MyButton text={buttontext1} />
+                <MyButton text={buttontext2} />
+                <MyButton text={buttontext3} />
             </div>
         </div>
     )
