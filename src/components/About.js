783e02489461a900be86ff323bee9db8edd6714c
tomatams <tomatams@gmail.com>
2023-01-09 14:27:15 +0100

    about page content

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,16 @@
 export default function About () {
   return (
-    <div>hello</div>
-    <div>hello</div>
+    <div>Legfontosabb céljaink:
+        <ul>
+            <li>Környezeti szemléletformálás ismeretterjesztő előadásokon, és tematikus nyári ifjúsági táborokban</li>
+            <li>A természetes környezet és tájkép védelme</li>
+            <li>Környezetkímélő gazdálkodási módok, természetvédelmi technikák kidolgozása és megvalósítása</li>
+            <li>Egyes veszélyeztetett állat és növényfajok gyakorlati védelme</li>
+            <li>Sérült, vadonélő védett állatok mentése, gyógykezelése és visszajuttatása a természetbe</li>
+            <li>Önkormányzatok természetvédelmi törekvéseinek támogatása</li>
+            <li>Védett és érzékeny természeti területek természetvédelmi kezelése</li>
+            <li>Kulturális és történelmi emlékhelyek védelme</li>
+        </ul>
+    </div>
   )
 }
