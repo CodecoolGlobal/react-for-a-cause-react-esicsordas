c42a2227d5cd030280bfed026f35fb572c77eedd
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-01-10 13:38:49 +0100

    Add non-working contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,19 @@
-export default function Contact () {
+export default function Contact() {
+  const clickContact = () => {
+    
+  };
   console.log("Contact");
+  let content = document.querySelector(".content");
+  content.innerHTML =
+    `<div>
+    <p id="welcometext"> Üzenjen nekünk!</p>
+    <form action="/action_page.php" method="get">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+      <button onClick=${clickContact}>Üzenet küldése</button>
+    </div>`
 }
