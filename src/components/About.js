535abfb243e94e6acfebb7d1c1a6f9f4329c6931
tomatams <tomatams@gmail.com>
2023-01-10 11:00:47 +0100

    about page function do works

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import dunakanyar from './Pictures_About/Dunakanyar.jpeg'
 import madar from './Pictures_About/madar.jpeg'
 
-
 export default function About () {
-  return (
-    <div className = "about-div">
+  console.log("About");
+  let content = document.querySelector(".content");
+  content.innerHTML = `<div className = "about-div">
       <h3 className = "about-h3">A Börzsöny Alapítványt 2003 évben hozták létre az MME Börzsönyi helyi csoportjának tagjai, a Börzsöny hegység és környéke természeti értékeinek védelmére, bemutatására és gyerekek táboroztatására.</h3>
       <h2 className = "about-h2">Legfontosabb céljaink:</h2>
         <div className = "about-div">
@@ -25,12 +25,11 @@ export default function About () {
         <h2 className = "about-h2">Programjaink:</h2>
         <h3 className = "about-h3">Szemétszedő akciók:</h3>
         <img className='about-img' src ="https://www.borzsony.org/media/user_photos/1708_5_27_354232.jpg" alt="borzsonytabor"></img>
-
         <div className = "about-div">
           A Föld Napja alkalmából erdőtakarítási napot rendezünk a Börzsöny valamely terhelt kiránduló területén. A Takarítási Világnap alkalmából Dunakeszin rendezünk hasonló akciót. Segítséget nyújtunk más szervezetek által rendezett környezettakarítási akciókhoz (jármű, védőeszköz, zsák biztosításával).
         </div>
         <h3 className = "about-h3">Ismeretterjesztő előadások:</h3>
-        <img className='about-img' src = {dunakanyar} alt="dunakanyar"></img>
+        <img className='about-img' src = {`dunakanyar`} alt="dunakanyar"></img>
 
         <div className = "about-div">
         Aktivistáink felkérésre diaképes, és projektoros előadásokat tartanak iskolákban, nyugdíjasklubokban, teleházban. A Börzsöny természeti értékeinek bemutatása mellett, a környezettudatos szemlélet fontosságára és a környezet sérülékenységére hívjuk fel a figyelmet, a környezetterhelő technológiák és helytelen vásárlási szokások bemutatásával. Iskolás csoportoknak "odúkopácsoló" gyakorlati órát tartunk, az elkészített odúkat elvihetik a gyerekek.
@@ -50,7 +49,7 @@ export default function About () {
         Felhívjuk azonban a figyelmet, hogy a nem védett madarakat (galambok, szarka, szajkó, dolmányos varjú, tőkés réce, stb.) sajnos nem áll módunkban fogadni ! 
         </div>
         <h3 className = "about-h3">Védett és fokozottan védett fajok felmérése, védelme:</h3>
-        <img className='about-img' src ={madar} alt="madar"></img>
+        <img className='about-img' src = {`madar`} alt="madar"></img>
         <div className = "about-div">
         Kameracsapdával monitorozzuk a Börzsöny rejtett életet élő vadfajait, elsősorban a hiúz és vadmacska kutatáshoz járulunk hozzá. Rendszeresen ellenőrizzük a börzsönyi erdőkben élő ritka madárfajokat (feketególya, fehérhátú harkály, vándorsólyom, uhu, macskabagoly stb.) Támogatjuk a MME Börzsönyi csoport madárodú telepítési programját.
         </div>
@@ -62,6 +61,5 @@ export default function About () {
         Kistó programunk során a Börzsönyi kétéltű állomány szaporodóhelyeinek felmérését, és néhány beiszapolódott víztest revitalizálását végeztük el.
         A Börzsöny területén GPS alkalmazásával felvételeztük a forrásokat, mértük vízminőségi és fizikai tulajdonságaikat, és adatszolgáltatást végeztünk a Nemzeti forráskataszter részére.
         </div>
-    </div>
-  )
+    </div>`
 }
