8c71b13c6861b6e6f3bb4433a46c800767ef711d
tomatams <tomatams@gmail.com>
2023-01-10 09:00:34 +0100

    div h1 ordering

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,24 @@
 export default function About () {
   return (
-    <div>Legfontosabb céljaink:
-        <ul>
-            <li>Környezeti szemléletformálás ismeretterjesztő előadásokon, és tematikus nyári ifjúsági táborokban</li>
-            <li>A természetes környezet és tájkép védelme</li>
-            <li>Környezetkímélő gazdálkodási módok, természetvédelmi technikák kidolgozása és megvalósítása</li>
-            <li>Egyes veszélyeztetett állat és növényfajok gyakorlati védelme</li>
-            <li>Sérült, vadonélő védett állatok mentése, gyógykezelése és visszajuttatása a természetbe</li>
-            <li>Önkormányzatok természetvédelmi törekvéseinek támogatása</li>
-            <li>Védett és érzékeny természeti területek természetvédelmi kezelése</li>
-            <li>Kulturális és történelmi emlékhelyek védelme</li>
-        </ul>
+    <div>
+      <h1>Legfontosabb céljaink:</h1>
+        <div>
+            <ul>
+                <li>Környezeti szemléletformálás ismeretterjesztő előadásokon, és tematikus nyári ifjúsági táborokban</li>
+                <li>A természetes környezet és tájkép védelme</li>
+                <li>Környezetkímélő gazdálkodási módok, természetvédelmi technikák kidolgozása és megvalósítása</li>
+                <li>Egyes veszélyeztetett állat és növényfajok gyakorlati védelme</li>
+                <li>Sérült, vadonélő védett állatok mentése, gyógykezelése és visszajuttatása a természetbe</li>
+                <li>Önkormányzatok természetvédelmi törekvéseinek támogatása</li>
+                <li>Védett és érzékeny természeti területek természetvédelmi kezelése</li>
+                <li>Kulturális és történelmi emlékhelyek védelme</li>
+            </ul>
+        </div>
+        <div>
+        Felismertük, hogy hosszútávon a környezet megóvásának a záloga, az új generációk környezeti nevelése, ezért elsőrendű feladatunknak tekintjük a fiatalok helyes szemléletének kialakítását. Programjaink végrehajtása során szorosan együttműködünk az MME 22. sz. Börzsönyi helyi csoportjával, és más civil szervezetekkel, valamint a Duna-Ipoly Nemzeti Parkkal, és az Ipoly-Erdő Zrt erdészeteivel. Alkalmazottunk nincs, minden tevékenységet önkéntesek látnak el.
+        </div>
+        <h1>Programjaink:</h1>
+        
     </div>
   )
 }
