7ff45662530bfcbe10fe19c83f50554e003b44c9
tomatams <tomatams@gmail.com>
2023-01-10 10:43:12 +0100

    pictures ready

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,7 @@
+import dunakanyar from './Pictures_About/Dunakanyar.jpeg'
+import madar from './Pictures_About/madar.jpeg'
+
+
 export default function About () {
   return (
     <div className = "about-div">
@@ -20,20 +24,20 @@ export default function About () {
         </div>
         <h2 className = "about-h2">Programjaink:</h2>
         <h3 className = "about-h3">Szemétszedő akciók:</h3>
-        <img src ="https://www.borzsony.org/media/user_photos/1708_5_27_354232.jpg" alt="borzsonytabor"></img>
+        <img className='about-img' src ="https://www.borzsony.org/media/user_photos/1708_5_27_354232.jpg" alt="borzsonytabor"></img>
 
         <div className = "about-div">
           A Föld Napja alkalmából erdőtakarítási napot rendezünk a Börzsöny valamely terhelt kiránduló területén. A Takarítási Világnap alkalmából Dunakeszin rendezünk hasonló akciót. Segítséget nyújtunk más szervezetek által rendezett környezettakarítási akciókhoz (jármű, védőeszköz, zsák biztosításával).
         </div>
         <h3 className = "about-h3">Ismeretterjesztő előadások:</h3>
-        <img src ="/components/Pictures_About/Dunakanyar.jpeg" alt="dunakanyar"></img>
+        <img className='about-img' src = {dunakanyar} alt="dunakanyar"></img>
 
         <div className = "about-div">
         Aktivistáink felkérésre diaképes, és projektoros előadásokat tartanak iskolákban, nyugdíjasklubokban, teleházban. A Börzsöny természeti értékeinek bemutatása mellett, a környezettudatos szemlélet fontosságára és a környezet sérülékenységére hívjuk fel a figyelmet, a környezetterhelő technológiák és helytelen vásárlási szokások bemutatásával. Iskolás csoportoknak "odúkopácsoló" gyakorlati órát tartunk, az elkészített odúkat elvihetik a gyerekek.
         </div>
         <h3 className = "about-h3">Nyári természetvédelmi táborok:</h3>
 
-        <img src ="https://www.borzsony.org/media/user_photos/1708_5_27_409683.jpg" alt="borzsonytabor"></img>
+        <img className='about-img' src ="https://www.borzsony.org/media/user_photos/1708_5_27_409683.jpg" alt="borzsonytabor"></img>
 
         <div className = "about-div">
         A több mint 30 éve magas színvonalon rendezett Börzsönyi Természetvédelmi Táborok további működését biztosítjuk, stabil anyagi és infrastruktúrális háttér biztosításával. A táborban, két turnusban kb. 100 iskolás vesz részt. A 12 napos turnusok minden napján, 10 fős kiscsoportokban tematikus foglalkozásokon mutatjuk be a természet titkait, és az erdőben viselkedés szabályait.
@@ -46,6 +50,7 @@ export default function About () {
         Felhívjuk azonban a figyelmet, hogy a nem védett madarakat (galambok, szarka, szajkó, dolmányos varjú, tőkés réce, stb.) sajnos nem áll módunkban fogadni ! 
         </div>
         <h3 className = "about-h3">Védett és fokozottan védett fajok felmérése, védelme:</h3>
+        <img className='about-img' src ={madar} alt="madar"></img>
         <div className = "about-div">
         Kameracsapdával monitorozzuk a Börzsöny rejtett életet élő vadfajait, elsősorban a hiúz és vadmacska kutatáshoz járulunk hozzá. Rendszeresen ellenőrizzük a börzsönyi erdőkben élő ritka madárfajokat (feketególya, fehérhátú harkály, vándorsólyom, uhu, macskabagoly stb.) Támogatjuk a MME Börzsönyi csoport madárodú telepítési programját.
         </div>
